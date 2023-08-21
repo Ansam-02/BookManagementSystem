901750d39a7bd13a430e059034e49484f8fe48c0
ansam <ansamjad02@gmail.com>
2023-08-21 17:19:05 +0300

    thr first

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var Book_js_1 = require("./routers/Book.js");
+export const __esModule = true;
+import express_1 from "express";
+import Book_js_1 from "./routers/Book.js";
 var app = (0, express_1["default"])();
 var PORT = 3000;
 app.use(express_1["default"].json());
